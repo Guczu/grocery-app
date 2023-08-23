10bb68695359ff44a5629548e58f27468bfcd43d
Guczu <jakub180720@gmail.com>
2023-08-23 20:32:01 +0200

    added business, middleware, dao, endpoints, services

---
+++ server/app/REST/user.endpoint.js
@@ -0,0 +1,35 @@
+import business from '../business/business.container.js';
+import applicationException from '../service/applicationException.js';
+
+const userEndpoint = (router) => {
+    router.post('/api/user/auth', async (request, response, next) => {
+        try {
+            let result = await business.getUserManager(request).authenticate(request.body.name, request.body.password);
+            response.status(200).send(result);
+        } catch (error) {
+            applicationException.errorHandler(error, response);
+        }
+    });
+
+    router.post('/api/user/create', async (request, response, next) => {
+        try {
+            const result = await business.getUserManager(request).createNewOrUpdate(request.body);
+            response.status(200).send(result);
+        } catch (error) {
+            applicationException.errorHandler(error, response);
+        }
+    });
+
+    router.get('/api/user', async (request, response, next) => {
+        try {
+            console.log('api get all users')
+            const result = await business.getUserManager(request).getAllUsers();
+            response.status(200).send(result);
+        } catch (error) {
+            applicationException.errorHandler(error, response);
+        }
+    });
+
+};
+
+export default userEndpoint;
--- server/app/app.js
+++ server/app/app.js
@@ -1,14 +1,22 @@
 import config from './config.js';
 import express from 'express';
 import mongoose from 'mongoose';
+import routes from './REST/routes.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({limit: '2048kb'}));
 
-mongoose.connect(config.databaseUrl)
+mongoose.connect(config.databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.info('Mongodb connected');
     })
     .catch(error => console.info(error));
+
+routes(app);
 
 app.listen(config.port, function () {
     console.info(`Server is running at ${config.port}`)
