e492b3a5c0a2775cd72f6db22e0276eea2aba23d
Guczu <jakub180720@gmail.com>
2023-10-15 14:55:07 +0200

    added slider functionalities, breakpoints

--- client/src/views/MostPopularSlider/MostPopularSlider.jsx
+++ client/src/views/MostPopularSlider/MostPopularSlider.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react"
 import { getMostPopular } from "../../services/products.service";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Scrollbar } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import Header from "../../components/Header/Header";
+import { LiaCartPlusSolid } from 'react-icons/lia'
+import CustomButton from "../../components/CustomButton/CustomButton";
+import saveToCart from "../../utils/saveToCart";
+import ProductsAddCartPopup from "../Products/ProductsAddCartPopup/ProductsAddCartPopup";
 
 const MostPopularSlider = () => {
     const [products, setProducts] = useState([]);
+    const [addProductPopup, setAddProductPopup] = useState(false);
 
     useEffect(() => {
         const fetchPopularProducts = async () => {
@@ -22,38 +27,85 @@ const MostPopularSlider = () => {
         fetchPopularProducts();
     },[])
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setAddProductPopup(false);
+        }, 3000);
+    
+        return () => clearTimeout(timer);
+      },[addProductPopup])
+
   return (
-    <section className="container mx-auto mt-12">
+    <>
+        {addProductPopup && <ProductsAddCartPopup/>}
+        <section className="container mx-auto mt-12">
 
-        <Header title={'Najpopularniejsze produkty'} styles={'text-heading-3 text-typography-text px-[16px]'}/>
-        <Header title={'wybierane przez klientów'} styles={'text-body-5 text-typography-subtext px-[16px]'}/>
+            <Header title={'Najpopularniejsze produkty'} styles={'text-heading-3 text-typography-text px-[16px]'}/>
+            <Header title={'wybierane przez klientów'} styles={'text-body-5 text-typography-subtext px-[16px]'}/>
 
-        <Swiper
-            modules={[Scrollbar]}
-            spaceBetween={20}
-            slidesPerView={5}
-            slidesPerGroup={1}
-            scrollbar={{ draggable: true, dragClass: 'swiper-scrollbar-drag' }}
-        >
-            {products && products.map((product, i) => (
-                <SwiperSlide 
-                    key={i}
-                    className="w-16 h-[300px] bg-white flex flex-col justify-center items-center rounded-[15px] hover:cursor-pointer mb-6"
-                >
-                    <div className="w-full h-2/5 bg-white">
-                        <img 
-                            src={product.image_url} 
-                            alt={product.product_name} 
-                            className="w-full h-full object-contain"
-                        />
-                    </div>
-                    <div className="w-full h-1/3 flex justify-center items-center">
-                        <span className="text-center text-heading-3">{product.product_name}</span>
-                    </div>
-                </SwiperSlide>
-            ))}
-      </Swiper>
-    </section>
+            <Swiper
+                modules={[Pagination, Navigation]}
+                spaceBetween={20}
+                slidesPerView={5}
+                slidesPerGroup={1}
+                navigation={true}
+                breakpoints={{
+                    0: {
+                      spaceBetween: 10,
+                      slidesPerView: 1,
+                    },
+                    640: {
+                      spaceBetween: 10,
+                      slidesPerView: 2,
+                    },
+                    768: {
+                      spaceBetween: 15,
+                      slidesPerView: 3,
+                    },
+                    1024: {
+                      spaceBetween: 15,
+                      slidesPerView: 4,
+                    },
+                    1280: {
+                      spaceBetween: 30,
+                      slidesPerView: 5,
+                    },
+                  }}
+            >
+                {products && products.map((product, i) => (
+                    <SwiperSlide 
+                        key={i}
+                        className="relative w-16 h-[300px] bg-white flex flex-col justify-center items-center rounded-[15px] mb-6"
+                    >
+                        <div className="w-full h-3/5 bg-white p-2">
+                            <img 
+                                src={product.image_url} 
+                                alt={product.product_name} 
+                                className="w-full h-full object-contain"
+                            />
+                        </div>
+
+                        <div className="w-full h-fit flex justify-center items-center">
+                            <span className="text-center text-heading-3">{product.product_name}</span>
+                        </div>
+
+                        <div className="absolute w-full h-full justify-center items-center">
+                            <CustomButton 
+                                onClick={() => {
+                                    saveToCart(product)
+                                    setAddProductPopup(true);
+                                }}
+                            >
+                                <div className='absolute top-0 right-0 m-2 w-[40px] h-[40px] p-2 flex flex-col justify-center items-center border-[2px] border-main-primary hover:bg-main-primary rounded-[10px]'>
+                                    <LiaCartPlusSolid className='absolute w-[25px] h-[25px]'/>
+                                </div>
+                            </CustomButton>
+                        </div>
+                    </SwiperSlide>
+                ))}
+        </Swiper>
+        </section>
+    </>
   )
 }
 
