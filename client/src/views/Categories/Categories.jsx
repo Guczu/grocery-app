9427d5220657550aab144c7d0f60764b0a0bee7e
Guczu <jakub180720@gmail.com>
2023-10-16 20:07:12 +0200

    fixed rwd

--- client/src/views/Categories/Categories.jsx
+++ client/src/views/Categories/Categories.jsx
@@ -3,7 +3,7 @@ import { categories } from "../../constants"
 
 const Categories = () => {
   return (
-    <section className="w-full py-3 bg-base-softbackground">
+    <section className="hidden md:flex w-full py-3 bg-base-softbackground">
         <div className="container h-fill md:h-[32px] mx-auto flex md:flex-row flex-col justify-evenly items-center">
             {categories.map((category, index) => (
               <CategoryLink key={index} category={category}/>
--- client/src/views/PageLayout/PageLayout.jsx
+++ client/src/views/PageLayout/PageLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../Navbar/Navbar"
-import Categories from "../Categories/Categories"
 import Footer from "../Footer/Footer"
 
 const PageLayout = () => {
   return (
     <>
         <Navbar/>
-        <Categories/>
         <Outlet/>
         <Footer/>
     </>
