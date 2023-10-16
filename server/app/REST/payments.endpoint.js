ac7e6afe402df9f6c1ff2b287a972fae27f2acb0
Guczu <jakub180720@gmail.com>
2023-10-16 19:03:16 +0200

    added auth middleware to payment endpoint

--- server/app/REST/payments.endpoint.js
+++ server/app/REST/payments.endpoint.js
@@ -1,9 +1,10 @@
 import business from '../business/business.container.js';
 import applicationException from '../service/applicationException.js';
+import auth from '../middleware/auth.js';
 
 const paymentsEndpoint = (router) => {
 
-    router.post('/api/payment/create-payment', async (request, response, next) => {
+    router.post('/api/payment/create-payment', auth, async (request, response, next) => {
         try {
             const result = await business.getPaymentsManager(request).createPayment(request.body);
             response.status(200).send(result.id);
